e37114b9487301c17b93dd45c689b9d3000be70a
leocorv <97278172+leocorv@users.noreply.github.com>
2023-03-10 10:54:07 +0100

    header "finit"

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -4,27 +4,26 @@
         <img class="h-auto w-72 dark:rounded-md" src="/src/assets/miliboo_logo.png" alt="Logo miliboo" >
         </div>
         <div class="flexbox relative w-20 img_nav">
-          <img class="block mt-6 w-14 text-center image"  src="/src/assets/logo_user.png">
-          <div class="absolute l-0 w-auto text-left opacity-0 font-bold transition-all text dark:text-white">Compte</div>
+          <img class="block mt-4 w-14 text-center image dark:invert contrast-50"  src="/src/assets/logo_user.png">
+          <div class="absolute l-0 w-auto text-left opacity-0 font-bold transition-all text dark:text-white font-semibold">Compte</div>
         </div>
         <div class="flexbox relative w-20 img_nav">
-          <img class="block mt-6 w-14 text-center image"  src="/src/assets/boutique.png">
-          <div class="absolute l-0 w-auto text-left opacity-0 font-bold transition-all text dark:text-white">Boutique</div>
+          <img class="block mt-4 w-14 text-center image dark:invert contrast-50"  src="/src/assets/boutique.png">
+          <div class="absolute l-0 w-auto text-left opacity-0 font-bold transition-all text dark:text-white font-semibold">Boutique</div>
         </div>
         <div class="flexbox relative w-20 img_nav">
-          <img class="block mt-6 w-14 text-center image"  src="/src/assets/caddie.png">
-          <div class="absolute ml-3 w-auto text-left opacity-0 font-bold transition-all text dark:text-white">Panier</div>
+          <img class="block mt-4 w-14 text-center image dark:invert contrast-50"  src="/src/assets/caddie.png">
+          <div class="absolute ml-3 w-auto text-left opacity-0 font-bold transition-all text dark:text-white font-semibold">Panier</div>
         </div>
         <div class="flexbox relative w-20 img_nav">
-          <img class="block mt-6 w-14 text-center image"  src="/src/assets/help.png">
-          <div class="absolute l-0 ml-3 text-center opacity-0 font-bold transition-all w-3 text dark:text-white">Aide</div>
+          <img class="block mt-4 w-14 text-center image dark:invert contrast-50"  src="/src/assets/help.png">
+          <div class="absolute l-0 ml-3 text-center opacity-0 font-bold transition-all w-3 text dark:text-white font-semibold">Aide</div>
         </div>
         <div class="search-container">
             <img src="/src/assets/loupe.png" alt="Loupe" @click="search()">
             <input type="text" v-model="searchTerm" placeholder="Rechercher..." @keydown.enter="search()">
         </div>
-    </div>  
-
+    </div> 
 </template>
   
 <script>   
@@ -76,11 +75,11 @@
   align-items: center;
   background-color: #f1f1f1;
   border-radius: 20px;
-  margin: 10px;
+  margin: 0.7rem;
   padding-right: 1rem;
   padding-left: 1rem;
-  width: 25%;
-  height: 5rem;
+  width: 20rem;
+  height: auto;
 }
 
 .search-container img {
