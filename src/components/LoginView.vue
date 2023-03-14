71139662fedbfbc5a9922c2d96a6cd3e321dd411
leocorv <97278172+leocorv@users.noreply.github.com>
2023-03-14 10:27:40 +0100

    Configuration du port

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="inline-flex justify-center text-center items-center flexbox p-5 font-sans w-full">
+    <div class="text-center flex flex-col items-center mb-8 p-5 font-sans w-full h-full">
+        <div class="flex flex-grow">
+            <span class="text-3xl font-medium text-gray-900 dark:text-gray-600 mr-3 mb-2">Se connecter</span>
+            <div>
+                <label class="relative inline-flex items-center mr-5 cursor-pointer">
+                    <input type="checkbox" value="" class="sr-only peer">
+                    <div style="width: 4.5rem;" class="h-9 bg-red-600 rounded-full peer dark:bg-red-400 peer-focus:ring-4 peer-focus:ring-yellow-300 dark:peer-focus:ring-gray-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-8 after:w-8 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-400"></div>
+                </label>
+            </div>
+            <span class=" pb-11 text-3xl font-medium text-gray-900 dark:text-gray-600">S'enregistrer</span>
+        </div>  
         <div class="text-center flex flex-col items-center mb-8">
             <h1 class="text-5xl font-semibold">Connexion</h1>
             <div class="inline-block justify-around text-center">
