b4210c9fdfd1717d17f4473c5ff9d559d529c87b
Cormac <X00179608@mytudublin.ie>
2023-03-14 16:59:34 +0000

    commit à partir du serveur

---
+++ src/components/account/Account_information.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="inline-flex justify-center w-full">
+        <div class="mt-6 text-center items-center">
+            <h1 class="text-5xl font-semibold">Mon compte</h1>
+            <div class=" p-2 mb-4 border-solid border-2 border-gray-500/10 rounded-xl shadow-xl w-full">
+                <div class="flex justify-between">
+                    <div>Informations personnelles</div> <div>Commandes</div> <div>Moyen de paiement</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/account/RegisterView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="register" class="text-center flex-col items-center mb-8 flex">
+    <h1 class="text-5xl font-semibold">S'enregistrer</h1>
+    <div class="inline-block justify-around text-center">
+        <div class="p-3 m-4 text-center items-center border-gray-500/10 border-2 border-solid rounded-3xl shadow-lg min-w-full w-80">
+            <h2 class="text-3xl font-semibold">Par adresse mail</h2>
+            <form method="post" action="#">
+                <div class="pt-4">
+                    <label>E-mail</label>
+                    <div>
+                        <input class="border-solid border-2 border-gray-700/10 min-w-full rounded-xl text-lg p-1" id="email" type="email" name="email" value required autocomplete="email" autofocus>
+                    </div>
+                </div>
+                <div class="pt-4">
+                    <label>Mot de passe</label>
+                    <div>
+                        <input class="border-solid border-2 border-gray-700/10 min-w-full rounded-xl text-lg p-1" id="password" type="password" name="password" value required autocomplete="current-password">
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" class=" rounded-full">S'enregistrer</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</template>
