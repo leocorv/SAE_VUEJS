088e8b07a0c9d8cdb7748f6024b68bacf6446439
leocorv <97278172+leocorv@users.noreply.github.com>
2023-03-14 17:30:46 +0100

    push sur info perso

--- src/components/account/Account_information.vue
+++ src/components/account/Account_information.vue
@@ -2,8 +2,10 @@
     <div class="inline-flex justify-center w-full">
         <div class="mt-6 text-center items-center">
             <h1 class="text-5xl font-semibold">Mon compte</h1>
-            <div class=" p-2 mb-4 border-solid border-2 border-gray-500/10 rounded-xl shadow-xl min-w-full w-80">
-                
+            <div class=" p-2 mb-4 border-solid border-2 border-gray-500/10 rounded-xl shadow-xl w-full">
+                <div class="flex justify-between">
+                    <div>Informations personnelles</div> <div>Commandes</div> <div>Moyen de paiement</div>
+                </div>
             </div>
         </div>
     </div>
